a77af9918e6d2c2710b4955f53dfcd820a3ad251
busiui-beck <beck@busiui.com>
2020-04-12 17:47:55 +0800

    table，select功能更新

--- src/components/busiui-table/busiui-select/busiui-select.component.ts
+++ src/components/busiui-table/busiui-select/busiui-select.component.ts
@@ -4,13 +4,15 @@ import { BusiUiComponent } from '../../busiui-component';
 const VIEW = require( './busiui-select.view.html');
 // import { BusiUiComponent } from './busiui-select.view.html';
 /**
- * 动态Grid
+ * 动态Grid-查询区域，查询条件输入框组
  */
 export class BusiUiTableSelect extends BusiUiComponent {
 
-
+    busiuiID:any;
     table: any; //表格jquery对象
-    selesctOpConfig: Array<BusiuiSelectOpConfigModel> = []; //查询区域配置
+    tabCode:any;
+    rowColMap:any = {};//行列控件位置map，用于判断控件div的宽度
+    selectOpConfig: Array<BusiuiSelectOpConfigModel> = []; //查询区域配置
     eventService: EventService; //通知事件
     queryDTO: any = {}; //查询区域DTO
     gridBtnConfig: Array<BusiuiGridBtnConfigModel> = []; //按钮设置表
@@ -21,93 +23,72 @@ export class BusiUiTableSelect extends BusiUiComponent {
     constructor() {
         // Always call super first in constructor
         super();
-
-        this.selesctOpConfig = [
-            {
-                id: 1,
-                func_id: 1,//              number(19)                     null,
-                select_code: "select_code",//          VARCHAR2(50)                   null,
-                input_type: "2",//           VARCHAR2(4)                    null,
-                input_row: 1,//            int                            null,
-                input_column: 1,//         int                            null,
-                input_dict: "select_code",//           VARCHAR2(200)                  null,
-                input_grouptype: "select_code",//      VARCHAR2(4)                    null,
-                input_group: 1,//          int                            null,
-                input_group_unitnum: 2,//  int                            null,
-                ng_model: "2",//             VARCHAR2(50)                   null,
-                placeholder: "select_code",//          VARCHAR2(200)                  null,
-                where_and: "select_code",//            VARCHAR2(200)                  null,
-                default_value: "select_code",//        VARCHAR2(50)                   null,
-                have_display: "select_code",//         VARCHAR2(4)                    null,
-                if_display: "select_code",//           VARCHAR2(100)                  null,
-                lable: "",//                VARCHAR2(200)                  null,
-            },
-            {
-                id: 1,
-                func_id: 1,//              number(19)                     null,
-                select_code: "select_code",//          VARCHAR2(50)                   null,
-                input_type: "13",//           VARCHAR2(4)                    null,
-                input_row: 1,//            int                            null,
-                input_column: 1,//         int                            null,
-                input_dict: "select_code",//           VARCHAR2(200)                  null,
-                input_grouptype: "select_code",//      VARCHAR2(4)                    null,
-                input_group: 1,//          int                            null,
-                input_group_unitnum: 2,//  int                            null,
-                ng_model: "13",//             VARCHAR2(50)                   null,
-                placeholder: "select_code",//          VARCHAR2(200)                  null,
-                where_and: "select_code",//            VARCHAR2(200)                  null,
-                default_value: "select_code",//        VARCHAR2(50)                   null,
-                have_display: "select_code",//         VARCHAR2(4)                    null,
-                if_display: "select_code",//           VARCHAR2(100)                  null,
-                lable: "test",//                VARCHAR2(200)                  null,
-            },
-            {
-                id: 1,
-                func_id: 1,//              number(19)                     null,
-                select_code: "select_code",//          VARCHAR2(50)                   null,
-                input_type: "12",//           VARCHAR2(4)                    null,
-                input_row: 1,//            int                            null,
-                input_column: 1,//         int                            null,
-                input_dict: "select_code",//           VARCHAR2(200)                  null,
-                input_grouptype: "select_code",//      VARCHAR2(4)                    null,
-                input_group: 1,//          int                            null,
-                input_group_unitnum: 2,//  int                            null,
-                ng_model: "12",//             VARCHAR2(50)                   null,
-                placeholder: "select_code",//          VARCHAR2(200)                  null,
-                where_and: "select_code",//            VARCHAR2(200)                  null,
-                default_value: "select_code",//        VARCHAR2(50)                   null,
-                have_display: "select_code",//         VARCHAR2(4)                    null,
-                if_display: "select_code",//           VARCHAR2(100)                  null,
-                lable: "test",//                VARCHAR2(200)                  null,
-            },
-            {
-                id: 1,
-                func_id: 1,//              number(19)                     null,
-                select_code: "select_code",//          VARCHAR2(50)                   null,
-                input_type: "11",//           VARCHAR2(4)                    null,
-                input_row: 1,//            int                            null,
-                input_column: 1,//         int                            null,
-                input_dict: "select_code",//           VARCHAR2(200)                  null,
-                input_grouptype: "select_code",//      VARCHAR2(4)                    null,
-                input_group: 1,//          int                            null,
-                input_group_unitnum: 2,//  int                            null,
-                ng_model: "11",//             VARCHAR2(50)                   null,
-                placeholder: "select_code",//          VARCHAR2(200)                  null,
-                where_and: "select_code",//            VARCHAR2(200)                  null,
-                default_value: "select_code",//        VARCHAR2(50)                   null,
-                have_display: "select_code",//         VARCHAR2(4)                    null,
-                if_display: "select_code",//           VARCHAR2(100)                  null,
-                lable: "test",//                VARCHAR2(200)                  null,
+        // 获取不到值，先写死
+        this.busiuiID = '14001';
+        this.tabCode = '[TAB][14001]-1';
+        let bizConf = BusiUiComponent.conf[this.busiuiID];
+        // console.log("===================================");
+        // console.log(JSON.stringify(bizConf));
+        // console.log("===================================");
+        //获取tab和select的关系
+        let tabSelRels = bizConf.BusiuiTabSelRelModel;
+        let currentSelCode = "";
+        for (let index in tabSelRels) {
+            //找到当前tabcode，查询对应的selectcode
+            if(tabSelRels[index].tab_code == this.tabCode){
+                currentSelCode = tabSelRels[index].select_code;
             }
-        ]
+        }
+        //加载对应tab的select
+        let selectOpConfigs = bizConf.BusiuiSelectOpConfigModel;
+        let rowColMap:any={};
+        //寻找当前tab所需要渲染的select组件，并准备所有组件所在位置的map
+        for(let index in selectOpConfigs){
+            if(selectOpConfigs[index].select_code == currentSelCode){
+                let inputRow = selectOpConfigs[index].input_row;
+                let inputCol = selectOpConfigs[index].input_column;
+                let cellIndex = inputRow+'-'+inputCol;
+                console.log(this.rowColMap.hasOwnProperty(cellIndex));
+                //是否显示为0时，不算该组件所占位置
+                if(selectOpConfigs[index].have_display != '0'){
+                    if(this.rowColMap.hasOwnProperty(cellIndex)){
+                        let cellNumber = this.rowColMap[cellIndex];
+                        if(cellNumber == undefined || cellNumber == null){
+                            cellNumber = 0;
+                        }
+                        cellNumber +=1;
+                        this.rowColMap[cellIndex] = cellNumber;
+                    }else{
+                        this.rowColMap[cellIndex] = 1;
+                    }
+                }
+
+                this.selectOpConfig.push(selectOpConfigs[index]);
+            }
+        }
     }
 
 
     updateStyle() {
         const shadow = this.shadowRoot;
         const selesctOp = [];
-        for (const key in this.selesctOpConfig) {
-            selesctOp.push(this.genComponent(this.selesctOpConfig[key]));
+        debugger;
+        for (const key in this.selectOpConfig) {
+            
+            let inputRow = this.selectOpConfig[key].input_row;
+            let inputCol = this.selectOpConfig[key].input_column;
+            let cellIndex = inputRow+'-'+inputCol;
+            let cellNumber = this.rowColMap[cellIndex];
+            if(cellNumber == 1){
+                selesctOp.push(this.genComponent(4,this.selectOpConfig[key]));
+            }else if(cellNumber == 2){
+                selesctOp.push(this.genComponent(2,this.selectOpConfig[key]));
+            }else if(cellNumber == 1){
+                selesctOp.push(this.genComponent(1,this.selectOpConfig[key]));
+            }else{
+                selesctOp.push(this.genComponent(4,this.selectOpConfig[key]));
+            }
+            
         }
         const html = this.render(VIEW.default, { select: selesctOp.join('') });
         this.$(shadow).find('div').html(html);
@@ -122,68 +103,16 @@ export class BusiUiTableSelect extends BusiUiComponent {
     //初始化Table
     Init() {
         const $ = this.$;
-        // const oTableInit: any = new Object();
-        const conf = {
-            url: './mock/demo.json',         //请求后台的URL（*）
-            method: 'get',                      //请求方式（*）
-            toolbar: '#toolbar',                //工具按钮用哪个容器
-            striped: true,                      //是否显示行间隔色
-            cache: false,                       //是否使用缓存，默认为true，所以一般情况下需要设置一下这个属性（*）
-            pagination: true,                   //是否显示分页（*）
-            sortable: false,                     //是否启用排序
-            sortOrder: "asc",                   //排序方式
-            queryParams: this.queryParams,//传递参数（*）
-            sidePagination: "server",           //分页方式：client客户端分页，server服务端分页（*）
-            pageNumber: 1,                       //初始化加载第一页，默认第一页
-            pageSize: 10,                       //每页的记录行数（*）
-            pageList: [10, 25, 50, 100],        //可供选择的每页的行数（*）
-            search: true,                       //是否显示表格搜索，此搜索是客户端搜索，不会进服务端，所以，个人感觉意义不大
-            strictSearch: true,
-            showColumns: true,                  //是否显示所有的列
-            showRefresh: true,                  //是否显示刷新按钮
-            minimumCountColumns: 2,             //最少允许的列数
-            clickToSelect: true,                //是否启用点击选中行
-            height: 500,                        //行高，如果没有设置height属性，表格自动根据记录条数觉得表格高度
-            uniqueId: "ID",                     //每一行的唯一标识，一般为主键列
-            showToggle: true,                    //是否显示详细视图和列表视图的切换按钮
-            cardView: false,                    //是否显示详细视图
-            detailView: false,                   //是否显示父子表
-            columns: [{
-                checkbox: true
-            }, {
-                field: 'Name',
-                title: '部门名称'
-            }, {
-                field: 'ParentName',
-                title: '上级部门'
-            }, {
-                field: 'Level',
-                title: '部门级别'
-            }, {
-                field: 'Desc',
-                title: '描述'
-            },]
-        };
-        // this.$(this.shadowRoot).find('#tb_departments').bootstrapTable(conf);
-        $(this.shadowRoot).find('#btn_query').click(() => {
-            console.log(1);
-            $(this.shadowRoot).find('#tb_departments').bootstrapTable('refresh', conf);
-        });
-        this.query(conf);
-        const obser = EventService.getEvent();
-        console.log(obser);
-        //统一事件接受器
-        obser.subscribe((event: any) => {
-            console.log(event);
-            for (const key in event) {
-                this.queryDTO[key] = event[key].target.value;
-            }
-        })
+        
     }
 
     query(conf: any) {
         console.log(this.table);
         this.table = this.$(this.shadowRoot).find('#tb_departments').bootstrapTable(conf);
+        console.log(this.table);
+    }
+    reset(conf: any) {
+        
         console.log(this.table);
     }
 
@@ -205,34 +134,51 @@ export class BusiUiTableSelect extends BusiUiComponent {
     * 生成组件
     * @param opConfig 
     */
-    genComponent(opConfig: BusiuiSelectOpConfigModel) {
+    genComponent(width:number,opConfig: BusiuiSelectOpConfigModel) {
         let ret;
-        const conf = JSON.stringify(opConfig);//{\"lable\":\"部门\",\"ngModel\":\"Name\"}
+        const conf = JSON.stringify(opConfig);
         switch (opConfig.input_type) {
             case '1':
+                ret = `<div class="col-md-`+width+`"><busiui-input-pwd class="form-group" conf=\'${conf}\'></busiui-input-pwd></div>`;
+                break;
             case '2':
+                ret = `<div class="col-md-`+width+`"><busiui-input class="form-group" conf=\'${conf}\'></busiui-input></div>`;
+                break;
             case '3':
+                ret = `<div class="col-md-`+width+`"><busiui-input-float class="form-group" conf=\'${conf}\'></busiui-input-float></div>`;
+                break;
             case '4':
-                ret = `<div class="col-md-4"><busiui-input class="form-group" conf=\'${conf}\'></busiui-input></div>`;
+                ret = `<div class="col-md-`+width+`"><busiui-input-int class="form-group" conf=\'${conf}\'></busiui-input-int></div>`;
                 break;
             case '5':
+                ret = `<div class="col-md-`+width+`"><busiui-select class="form-group" conf=\'${conf}\'></busiui-select></div>`;
+                break;
             case '6':
-                ret = `<div class="col-md-4"><busiui-checkbox class="form-group" conf=\'${conf}\'></busiui-checkbox></div>`;
+                ret = `<div class="col-md-`+width+`"><busiui-select class="form-group" conf=\'${conf}\'></busiui-select></div>`;
                 break;
             case '7':
+                ret =`<div class="col-md-`+width+`"><busiui-datetimepicker class="form-group" conf=\'${conf}\'></busiui-datetimepicker></div>`;
+                break;
             case '8':
+                ret =`<div class="col-md-`+width+`"><busiui-datetimepicker class="form-group" conf=\'${conf}\'></busiui-datetimepicker></div>`;
+                break;
             case '9':
+                ret =`<div class="col-md-`+width+`"><busiui-datetimepicker class="form-group" conf=\'${conf}\'></busiui-datetimepicker></div>`;
+                break;
             case '10':
-                ret = `<div class="col-md-4"><busiui-checkbox class="form-group" conf=\'${conf}\'></busiui-checkbox></div>`;
+                ret = `<div class="col-md-`+width+`"><busiui-datetimepicker class="form-group" conf=\'${conf}\'></busiui-datetimepicker></div>`;
                 break;
             case '11':
-                ret = `<div class="col-md-4"><busiui-checkbox class="form-group" conf=\'${conf}\'></busiui-checkbox></div>`;
+                ret = `<div class="col-md-`+width+`"><busiui-checkbox class="form-group" conf=\'${conf}\'></busiui-checkbox></div>`;
                 break;
             case '12':
-                ret = `<div class="col-md-4"><busiui-radio class="form-group" conf=\'${conf}\'></busiui-radio></div>`;
+                ret = `<div class="col-md-`+width+`"><busiui-radio class="form-group" conf=\'${conf}\'></busiui-radio></div>`;
                 break;
             case '13':
-                ret = `<div class="col-md-4"><busiui-select class="form-group" conf=\'${conf}\'></busiui-select></div>`;
+                ret = `<div class="col-md-`+width+`"><busiui-select class="form-group" conf=\'${conf}\'></busiui-select></div>`;
+                break;
+            case '18':
+                ret = `<div class="col-md-`+width+`"><busiui-datetimepicker class="form-group" conf=\'${conf}\'></busiui-datetimepicker></div>`;
                 break;
         }
         return ret;
